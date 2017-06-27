5cd52aa679a9795cac5a284684dc843ee9740f34
ravindra0404 <ravindra.s@sonata-software.com>
2017-06-27 17:09:11 +0530

    Added Successfully

---
+++ gitDemo.java
@@ -0,0 +1,10 @@
+public class gitDemo{
+
+	public static void main(String[] args) {
+		System.out.println("Example for Git Demo");
+		for(int i=0;i<10;i++){
+			System.out.println("Increamented number is "+i);	
+		}
+	}
+
+}
---
+++ gitDemoOne.java
@@ -0,0 +1,10 @@
+public class gitDemoOne{
+
+	public static void main(String[] args) {
+	System.out.println("This line is added newly");
+		System.out.println("Example for Git Demo");
+		System.out.println("I changed again can you knew it, OK");
+System.out.println("Added again for this example");
+	}
+
+}
